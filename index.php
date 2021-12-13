3c171879866c4a82ea678efd0e583149df0e46b0
jorge <jorge@ruukfy.io>
2021-12-13 08:13:01 +0100

    [Moved] Schema Compiler bootstrap

--- index.php
+++ index.php
@@ -1,32 +1,19 @@
 <?php
 
+use Acme\Compiler;
 use Acme\DbLogger;
 use Acme\Entity\Comment;
 use Acme\Entity\Post;
 use Acme\Entity\User;
-use Acme\PHPSchemaDumper;
 use Acme\VO\Comment as CommentVO;
 use Acme\VO\Id;
-use Cycle\Annotated\Embeddings;
-use Cycle\Annotated\Entities;
-use Cycle\Annotated\MergeColumns;
-use Cycle\Annotated\MergeIndexes;
 use Cycle\Database\DatabaseManager;
 use Cycle\ORM\Factory;
 use Cycle\ORM\ORM;
 use Cycle\ORM\Schema;
 use Cycle\ORM\Select;
-use Cycle\Schema\Compiler;
-use Cycle\Schema\Generator\GenerateRelations;
-use Cycle\Schema\Generator\GenerateTypecast;
-use Cycle\Schema\Generator\RenderRelations;
-use Cycle\Schema\Generator\RenderTables;
-use Cycle\Schema\Generator\ResetTables;
-use Cycle\Schema\Generator\SyncTables;
-use Cycle\Schema\Generator\ValidateEntities;
-use Cycle\Schema\Registry;
-use Spiral\Tokenizer\ClassLocator;
-use Symfony\Component\Finder\Finder;
+
+
 
 require_once "vendor/autoload.php";
 
@@ -35,35 +22,9 @@ $dbal = new DatabaseManager(include_once('config/db.php'));
 $dbal->setLogger(new DbLogger());
 
 
-function compile($dbal): array
-{
-    $registry = new Registry($dbal);
-    $finder = (new Finder())->files()->in(['src/Acme/Entity']); // __DIR__ here is folder with entities
-    $classLocator = new ClassLocator($finder);
-    $cmp = new Compiler();
-    $cmp->compile($registry, [
-        new ResetTables(),       // re-declared table schemas (remove columns)
-        new Embeddings($classLocator),  // register embeddable entities
-        new Entities($classLocator),    // register annotated entities
-        new MergeColumns(),             // add @Table column declarations
-        new GenerateRelations(), // generate entity relations
-        new ValidateEntities(),  // make sure all entity schemas are correct
-        new RenderTables(),      // declare table schemas
-        new RenderRelations(),   // declare relation keys and indexes
-        new MergeIndexes(),             // add @Table column declarations
-        new SyncTables(),        // sync table changes to database
-        new GenerateTypecast(),  // typecast non string columns
-    ]);
-    $schema = $cmp->getSchema();
-
-    PHPSchemaDumper::dump($schema, 'config/schemas.php');
-
-    return $schema;
-}
-
 $schema = include('config/schemas.php');
 
-$schema = compile($dbal);
+$schema = Compiler::compile($dbal,'config/schemas.php');
 
 
 $orm = new ORM(new Factory($dbal), new Schema($schema));
---
+++ src/Acme/Compiler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Acme;
+
+use Cycle\Annotated\Embeddings;
+use Cycle\Annotated\Entities;
+use Cycle\Annotated\MergeColumns;
+use Cycle\Annotated\MergeIndexes;
+use Cycle\Schema\Compiler as CycleCompiler;
+use Cycle\Schema\Generator\GenerateRelations;
+use Cycle\Schema\Generator\GenerateTypecast;
+use Cycle\Schema\Generator\RenderRelations;
+use Cycle\Schema\Generator\RenderTables;
+use Cycle\Schema\Generator\ResetTables;
+use Cycle\Schema\Generator\SyncTables;
+use Cycle\Schema\Generator\ValidateEntities;
+use Cycle\Schema\Registry;
+use Spiral\Tokenizer\ClassLocator;
+use Symfony\Component\Finder\Finder;
+
+class Compiler
+{
+    public static function compile($dbal,$output=null): array
+    {
+        $registry = new Registry($dbal);
+        $finder = (new Finder())->files()->in(['src/Acme/Entity']); // __DIR__ here is folder with entities
+        $classLocator = new ClassLocator($finder);
+        $cmp = new CycleCompiler();
+        $cmp->compile($registry, [
+            new ResetTables(),       // re-declared table schemas (remove columns)
+            new Embeddings($classLocator),  // register embeddable entities
+            new Entities($classLocator),    // register annotated entities
+            new MergeColumns(),             // add @Table column declarations
+            new GenerateRelations(), // generate entity relations
+            new ValidateEntities(),  // make sure all entity schemas are correct
+            new RenderTables(),      // declare table schemas
+            new RenderRelations(),   // declare relation keys and indexes
+            new MergeIndexes(),             // add @Table column declarations
+            new SyncTables(),        // sync table changes to database
+            new GenerateTypecast(),  // typecast non string columns
+        ]);
+        $schema = $cmp->getSchema();
+
+        PHPSchemaDumper::dump($schema, $output);
+
+        return $schema;
+    }
+}
